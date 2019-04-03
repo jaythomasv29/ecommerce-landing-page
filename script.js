aaccbd8901580ec4fa0f3ca5cd99f8dfd2dd1e45
James Vongampai <jaythomasv29@gmail.com>
2019-04-03 09:47:53 -0700

    added landing page markup and css, and nav effect with responsiveness at different max widths

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function () {
+    // click function to toggle class
+    $('.menu-toggle, .fa-times').on('click', function () {
+        $('nav').toggleClass('active');
+        $('.overlay').toggleClass('menu-open');
+    });
+
+    $('.overlay').on('click', function(){
+        $('nav').removeClass('active');
+        $(this).removeClass('menu-open');
+    });
+});
